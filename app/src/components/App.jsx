a8f05388eb2c8c14565d94c2c857f13164aadab2
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2025-03-15 00:05:29 -0400

    init

---
+++ app/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../index.css";
+
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world! This is a React app with Tailwind CSS.
+    </h1>
+  );
+};
+
+export default App;
